69013c54a0dbfac5c354194bded6e98adad5b0c0
lucadra <luca.draisci@gmail.com>
2023-01-25 15:33:16 +0100

    removed many immaginy

--- main.js
+++ main.js
@@ -13,23 +13,26 @@ d3.csv("data/catalogue.csv", (d) => d).then(function(data) {
 
     const root = d3.select("#showcase")
 
-    // add total number of images to the group
+    // add total duration of each object
     let groups = d3.groups(data, (d) => d.object_name)
                    .map((d) => [d[0], d[1], d[1].length])
-                   .map((d) => [d[0], d[1].sort((a, b) => b.duration - a.duration).slice(0, 100), d[2]])
+                   .map((d) => [d[0], d[1], d[2], d3.sum(d[1], (d) => d.duration)])
+                   .map((d) => [d[0], d[1].sort((a, b) => b.duration - a.duration).slice(0, 100), d[2], d[3]])
                    .sort((a, b) => d3.sum(b[1], (d) => d.duration) - d3.sum(a[1], (d) => d.duration))
                    .forEach((d) => {
 
         let group = root.append("div").attr("class", "group")
         
         let text = group.append("div").attr("class", "text")
-        let collection = group.append("div").attr("class", "images")
+        let scrollBox = group.append("div").attr("class", "scrollBox")
+        let collection = scrollBox.append("div").attr("class", "images")
 
         d[1].forEach((d, i) => {
             // We take only the first 100 images to avoid turning our RAM into a toaster
             if (i > 100) return
             let container = collection
                                 .append("div")
+                                .attr("class", "container")
                                 .append("a")
                                 .attr("href", getImgPath(d))
                 
@@ -43,11 +46,9 @@ d3.csv("data/catalogue.csv", (d) => d).then(function(data) {
             images
                 .on("mouseover", function(e) {
                     let parent = d3.select(this.parentNode.parentNode.parentNode)
-                    console.log(parent.node().getBoundingClientRect())
 
                     d3.select(this)
                     .attr("height", `${parent.node().getBoundingClientRect().height}px`)
-
 
                     d3.select('body')
                         .append("div")
@@ -72,7 +73,14 @@ d3.csv("data/catalogue.csv", (d) => d).then(function(data) {
         text.append("p")
             .text(`${d[2]} images`)
             .style("font-size", "1em")
-            .style("margin-top", "0.2em")
+            .style("margin-top", "0em")
+            .style("margin-bottom", "0.2em")
+
+        text.append("p")
+            .text(`${secondsToMinutes(Math.round(d[3]))} total screen time`)
+            .style("font-size", "1em")
+            .style("margin-top", "0em")
+            .style("margin-bottom", "1.2em")
                         
     })
 })
